d614be5664ee0643f0e937c680ca0a38b1b2a214
bruunRasmus <gzj557@alumni.ku.dk>
2023-12-08 11:08:09 +0100

    refactored day8

--- day8/aoc_8.py
+++ day8/aoc_8.py
@@ -6,44 +6,26 @@ f = open(sys.argv[1],'r')
 def rlToInt (rlstring):
     return [1 if (c == 'R') else 0 for c in rlstring]
 
-rl = rlToInt(f.readlines(2)[0].rstrip())
-d = {}
-for l in f:
-    if l[0]!= '\n':
-        d[l[:3]] = [l.split("(")[1][:3],l[-5:-2]]
-#Part 1
-current = 'AAA'
-i = 0
-while current != 'ZZZ':
-    for walk in rl:
-        if current != 'ZZZ':
-            current = d[current][walk]
-            i+=1
-        else:
-            break
-print(i)
-currentMany  = [k for k in d if k[2]=='A']
-# Part 2
-def loopCountAndZs(start): 
-    zs = []
-    i = 0
-    current = start
-    flag = True
-    while flag:
+def findPathLength(start,endFun):
+    ends = [s for s in  d if endFun(s)]
+    i,current = (0,start)
+    while current not in ends:
         for walk in rl:
-            #print(current)
             current = d[current][walk]
             i+=1
-            if current[2] == 'Z':
-                if current in [z[1] for z in zs]:
-                    flag = False
-                else:
-                    zs.append([i,current])
-            if current == start:
-                flag = False
-                break
-    return (zs[0][0]) #they never visit more than 1 z before looping
+    return i
 
-z = [loopCountAndZs(c) for c in currentMany]
-print(np.lcm.reduce(z))
+rl = rlToInt(f.readline().rstrip()) #Left/Right string
+d = {l[:3]:[l.split("(")[1][:3],l[-5:-2]] for l in f if l[0]!= '\n'} #Dict of edges
+
+#Part 1
+print(findPathLength('AAA',lambda s:s=='ZZZ')) #Walk from AAA to ZZZ
+
+#Part 2
+currentMany  = [k for k in d if k[2]=='A'] #Walks from **A to **Z
+#Walks |s1->z1| = n, |s2->z2|= m => lcm(n,m) is |walk| from both s1->z1 and s2->z2,
+#if any walk from z1 cycles back to z1 in n steps, likewise for z2. So input is special case
+print(np.lcm.reduce(([findPathLength(c,lambda s: s[2]=='Z') for c in currentMany]))) 
+
+
        
