89e632855c42629809239c66777c036d97980957
bruunRasmus <gzj557@alumni.ku.dk>
2023-12-17 20:15:02 +0100

    day17

--- day17/aoc_17_alt.py
+++ day17/aoc_17_alt.py
@@ -6,7 +6,7 @@ heatmap = [[int(c) for c in l ] for l in heatmap]
 R= len(heatmap)
 C = len(heatmap[0])
 
-Q = {}
+Q = set()
 dist = {}
 prev = {}
 for indir in range(4):
@@ -14,7 +14,7 @@ for indir in range(4):
         for c in range(len(heatmap[0])):
             for r in range(len(heatmap)):
                 key = (r,c,l,indir)
-                Q[key] = 1
+                Q.add(key)
                 dist[key] = 1e10
                 prev[key] = [-1,-1]
 dist[(0,0,0,0)] = 0
@@ -22,39 +22,49 @@ dist[(0,0,0,1)] = 0
 dist[(0,0,0,2)] = 0
 dist[(0,0,0,3)] = 0
             
-
 def neighbour(x,d,l):
     c,r = x
     ns =  [(c+1,r,1),(c-1,r,3),(c,r+1,0),(c,r-1,2)]
     ns = [(cc,rr,dd,(l+1)) if d==dd else (cc,rr,dd,0) for cc,rr,dd in ns]
     return ns
-    
+
+removed = set()
 
 #print(Q)
 while len(Q)>0:
     #print("1")
     #print(len(Q))
-    mval = 1e6
-    mq = 0
+    #mval = 1e6
+    mval = 1e10
+    mq = -1
+    #print(len(Q))
     for q in Q:
-        if Q[q] == 1:
-            if dist[q] < mval:
-                mval = dist[q]
-                mq = q
+        if dist[q]<mval:
+            mval = dist[q]
+            mq = q
+    #    if dist[q] < mval:
+    #        mval = dist[q]
+    #        mq = q
     #print(dist[mq])
     #print(mq)
     r,c,l,d = mq
+    Q.remove(mq)
+    removed.add(mq)
     if [r,c] == [len(heatmap)-1,len(heatmap[0])-1]:
         print("YeS")
         break
-    Q[(r,c,l,d)] = 0
+    
     for n in neighbour((c,r),d,l):
         cc,rr,dd,ll = n
         if 0<=cc<C and 0<=rr<R and ll < 3 and abs(dd-d)!=2:
             alt = dist[(r,c,l,d)] + heatmap[r][c]
-            if alt < dist[(rr,cc,ll,dd)]:
-                dist[(rr,cc,ll,dd)] = alt
-                prev[(rr,cc,ll,dd)] = (r,c,l,d)
+            key = (rr,cc,ll,dd)
+            if key not in removed:
+                Q.add(key)
+                if alt < dist[key]:
+                    dist[key] = alt
+                    prev[key] = (r,c,l,d)
+
              
 minn = 1e5      
 I = -1
