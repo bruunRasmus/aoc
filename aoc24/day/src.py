602d0b450b0e71f893db59d1fba1c78450a8e1d9
bruunRasmus <gzj557@alumni.ku.dk>
2024-12-02 07:26:19 +0100

    day2

--- aoc24/day/src.py
+++
@@ -1,2 +0,0 @@
-﻿import sys
-with open(sys.argv[1],'r') as f:\n\t
--- aoc24/day2/src.py
+++ aoc24/day2/src.py
@@ -1,3 +1,13 @@
 ﻿import sys
 with open(sys.argv[1],'r') as f:
-    pass
+    nums = [[int(x) for x in  (l.rstrip().split(' '))] for l in f]
+
+def safe(xs,j):
+    xs  = xs[:j] + xs[j+1:] if j>=0 else xs
+    return True not in [xs[i+1]>=xs[i] or xs[i+1]+3<xs[i] for i in range(len(xs)-1)]
+
+s1,s2 = 0,0  
+for xs in nums:
+    s1+=(safe(xs,-1) or safe(list(reversed(xs)),-1))
+    s2+= True in [(safe(xs,i) or safe(list(reversed(xs)),i)) for i in range(len(nums))]
+print(s1,s2)
