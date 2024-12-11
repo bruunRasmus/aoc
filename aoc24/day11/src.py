873a3ef84820a8a9ed84cd9fd8f7580732c8d0bb
Rasmus <rasmusbruunjensen@gmail.com>
2024-12-11 07:25:43 +0100

    day11

--- aoc24/day11/src.py
+++ aoc24/day11/src.py
@@ -1,3 +1,27 @@
 ﻿import sys
 with open(sys.argv[1],'r') as f:
-    pass
+    D = f.read().strip()
+    stones = D.split()
+
+dp = {}
+def solve(s,t):
+    if (s,t) in dp.keys():
+        return dp[(s,t)]
+    if t == 0:
+        retval = 1
+    elif s == '0':
+        retval = solve('1',t-1)
+    elif len(s) % 2 == 0:
+        mid = len(s)//2
+        retval = solve(str(int(s[:mid])),t-1)  + solve(str(int(s[mid:])),t-1) 
+    else:
+        retval = solve(str(int(s)*2024),t-1)
+    
+    dp[(s,t)] = retval
+    return retval
+
+s1,s2 = 0,0
+for s in stones:
+    s1+=solve(s,25)
+    s2+=solve(s,75)
+print(s1,s2)
