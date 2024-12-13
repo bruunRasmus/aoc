8e230043a6e8e7e1475b99ca25e8902d231c75fb
Rasmus <rasmusbruunjensen@gmail.com>
2024-12-13 07:49:46 +0100

    day13, algebra in the morning is rough

--- aoc24/day13/src.py
+++ aoc24/day13/src.py
@@ -1,3 +1,22 @@
 ﻿import sys
+import re
 with open(sys.argv[1],'r') as f:
-    pass
+    D = f.read().strip()
+    games = D.split('\n\n')
+    
+config = [[int(x) for x in (re.findall('\d+',g))] for g in games]
+
+for part2 in [False,True]:
+    s = 0
+    for a1,a2,b1,b2,c1,c2 in config:
+        if part2:
+            c1 += 10000000000000
+            c2 += 10000000000000
+  
+        x2 = (c2-((a2/a1)*c1))/(b2-((a2/a1)*b1))
+        x1 = (c1-(b1*x2))/a1
+
+        x1,x2 = round(x1),round(x2)
+        if (x1*a1+x2*b1,x1*a2+x2*b2) == (c1,c2):
+            s += 3*x1+x2
+    print(s)
