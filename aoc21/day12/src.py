6bf7e8838e8dbd3bea07c8b9b11cf3161fd16f37
bruunRasmus <gzj557@alumni.ku.dk>
2025-12-21 20:57:23 +0100

    21, day12

--- aoc21/day12/src.py
+++ aoc21/day12/src.py
@@ -6,15 +6,15 @@ for l in lines:
     u,v = l.split("-")
     G[u].append(v)
     G[v].append(u)
-
-
-def solve(node,visited,twice,part2):
+    
+DP = {}
+def solve(node = "start",visited = [],twice = False,part2 = False):
     if node in visited and node == node.lower():
         twice = True
     visited_tuple = tuple(sorted(visited))
-    if (node,visited_tuple,twice) not in DP:
+    if (node,visited_tuple,twice,part2) not in DP:
         if node == "end":
-            DP[((node,visited_tuple,twice))] = 1
+            DP[((node,visited_tuple,twice,part2))] = 1
         else:
             paths = 0
             for u in G[node]:
@@ -22,13 +22,11 @@ def solve(node,visited,twice,part2):
                     paths += solve(u,visited + [node],twice,part2)
                 elif not(u == u.lower() and u in visited ): 
                     paths += solve(u,visited + [node],twice,part2)
-            DP[(node,visited_tuple,twice)] = paths
-    return DP[(node,visited_tuple,twice)]
+            DP[(node,visited_tuple,twice,part2)] = paths
+    return DP[(node,visited_tuple,twice,part2)]
 
-DP = {}
-ans1 = solve("start",[],False,part2=False)
-DP = {}
-ans2 = solve("start",[],False,part2=True)
+ans1 = solve(part2=False)
+ans2 = solve(part2=True)
 
 print(ans1)
 print(ans2)
